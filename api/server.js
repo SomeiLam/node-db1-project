a2929812728c5c75f47714feec8b6e6b8fc69526
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-10-04 13:38:48 -0700

    get all accounts from db

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
-
+const { logger } = require('./general-middleware')
+const AccountRouter = require("./accounts/accounts-router.js");
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", AccountRouter);
+
+server.get('/', logger, (req, res) => {
+    res.send(`<h2>Account API<h2>`);
+});
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: "not found"
+    });
+});
 
 module.exports = server;
